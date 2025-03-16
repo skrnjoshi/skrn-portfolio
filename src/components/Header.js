e8f383ed4f617732753eba34f9957b07efd413ce
Saikiran Donkana <saikirandonkana@Saikirans-MacBook-Air.local>
2025-03-16 05:48:03 +0530

    8th commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { HashLink as Link } from "react-router-hash-link";
-import logo from "../assets/img/skrnlogo.png"; // Adjust path accordingly
+import logo from "../assets/img/skrnlogo.png";
 import DarkModeToggle from "./DarkModeToggle";
 
 const Header = () => {
@@ -17,7 +17,7 @@ const Header = () => {
   return (
     <header className="l-header">
       <nav className="nav bd-grid">
-        <div>
+        <div className="nav-container-logo">
           <Link to="#" className="nav__logo">
             <img
               src={logo}
@@ -27,48 +27,57 @@ const Header = () => {
             />
           </Link>
         </div>
-        <div className={`nav__menu ${showMenu ? "show" : ""}`} id="nav-menu">
-          <ul className="nav__list">
-            <li className="nav__item">
-              <Link
-                to="#home"
-                className="nav__link active-link"
-                onClick={hideMenu}
-              >
-                Home
-              </Link>
-            </li>
-            <li className="nav__item">
-              <Link to="#about" className="nav__link" onClick={hideMenu}>
-                About
-              </Link>
-            </li>
-            <li className="nav__item">
-              <Link to="#skills" className="nav__link" onClick={hideMenu}>
-                Skills
-              </Link>
-            </li>
-            <li className="nav__item">
-              <Link to="#projects" className="nav__link" onClick={hideMenu}>
-                Projects
-              </Link>
-            </li>
-            <li className="nav__item">
-              <Link to="#work" className="nav__link" onClick={hideMenu}>
-                Work
-              </Link>
-            </li>
-            <li className="nav__item">
-              <Link to="#contact" className="nav__link" onClick={hideMenu}>
-                Contact
-              </Link>
-            </li>
-          </ul>
-          <DarkModeToggle />
-        </div>
-        <div className="nav__toggle" id="nav-toggle" onClick={toggleMenu}>
-          <DarkModeToggle />
-          <i className="bx bx-menu" id="icon_toggle"></i>
+
+        <div className="header-right">
+          {/* Always visible dark mode toggle */}
+
+          {/* Navigation menu */}
+          <div className={`nav__menu ${showMenu ? "show" : ""}`} id="nav-menu">
+            <ul className="nav__list">
+              <li className="nav__item">
+                <Link
+                  to="#home"
+                  className="nav__link active-link"
+                  onClick={hideMenu}
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="nav__item">
+                <Link to="#about" className="nav__link" onClick={hideMenu}>
+                  About
+                </Link>
+              </li>
+              <li className="nav__item">
+                <Link to="#skills" className="nav__link" onClick={hideMenu}>
+                  Skills
+                </Link>
+              </li>
+              <li className="nav__item">
+                <Link to="#projects" className="nav__link" onClick={hideMenu}>
+                  Projects
+                </Link>
+              </li>
+              <li className="nav__item">
+                <Link to="#work" className="nav__link" onClick={hideMenu}>
+                  Work
+                </Link>
+              </li>
+              <li className="nav__item">
+                <Link to="#contact" className="nav__link" onClick={hideMenu}>
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="darkmode-container">
+            <DarkModeToggle />
+          </div>
+
+          {/* Mobile menu toggle button */}
+          <div className="nav__toggle" id="nav-toggle" onClick={toggleMenu}>
+            <i className="bx bx-menu"></i>
+          </div>
         </div>
       </nav>
     </header>
